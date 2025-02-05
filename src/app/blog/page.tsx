2ec11b285a3e3032dbd98d841c7f7605578b7a0f
Wesley McGhee <wesmcghee6372@gmail.com>
2025-02-05 12:22:17 -0600

    fixed navbar

---
+++ src/app/blog/page.tsx
@@ -0,0 +1,11 @@
+import { Navbar } from "@/components/NavBar/Navbar.component";
+import React from "react";
+
+export default function Blog() {
+  return (
+    <div>
+      <Navbar />
+      <h1 style={{ textAlign: "center" }}>Under Construction</h1>
+    </div>
+  );
+}
--- src/components/NavBar/Navbar.component.tsx
+++ src/components/NavBar/Navbar.component.tsx
@@ -1,45 +1,39 @@
 "use client";
 
+import { Book, CaretDown, CaretUp, House } from "@phosphor-icons/react";
 import React, { useState } from "react";
 import styles from "./Navbar.module.css";
-import { Book, CaretDown, CaretUp, House } from "@phosphor-icons/react";
 import Link from "next/link";
 
 export function Navbar() {
   const [isOpen, setOpen] = useState<boolean>(false);
 
-  function handleExpand() {
+  function handleToggle() {
     setOpen(!isOpen);
   }
 
   return (
-    <div
-      className={`${styles.navbarContainer} ${
-        isOpen ? styles.navbarContainerOpened : styles.navbarContainerClosed
-      }`}
-    >
-      <div>
-        <h1 className={styles.logo}>WM</h1>
-        <div className={styles.expandContainer} onClick={handleExpand}>
+    <div className={`${styles.navbar} ${isOpen ? styles.navbarOpen : styles.navbarClosed}`}>
+      <div className={styles.logoContainer}>
+        <h1>WM</h1>
+        <div onClick={handleToggle}>
           {isOpen ? <CaretUp size={20} /> : <CaretDown size={20} />}
         </div>
       </div>
-      <div>
-        {isOpen && (
-          <div className={styles.linksContainer}>
-            <Link href="/">
-              <div className={styles.linkContainer}>
-                <House size={35} color="white" />
-              </div>
-            </Link>
-            <Link href="/blog">
-              <div className={styles.linkContainer}>
-                <Book size={35} color="white" />
-              </div>
-            </Link>
-          </div>
-        )}
-      </div>
+      {isOpen && (
+        <div className={styles.linksContainer}>
+          <Link href="/">
+            <div className={styles.linkContainer}>
+              <House size={35} />
+            </div>
+          </Link>
+          <Link href="/blog">
+            <div className={styles.linkContainer}>
+              <Book size={35} />
+            </div>
+          </Link>
+        </div>
+      )}
     </div>
   );
 }
